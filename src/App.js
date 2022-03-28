3d82b928afa7d2d93e676659c95e668e584daa0d
dev.k <dev.krecord@gmail.com>
2022-03-28 16:10:24 +0900

    feat: react에서 api 호출하기

--- src/App.js
+++ src/App.js
@@ -1,50 +1,75 @@
 import './App.css';
-import { useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import DiaryEditor from './DiaryEditor';
 import DiaryList from './DiaryList';
 
-// const dummyList = [
-//   {
-//     id: 1,
-//     author: 'dev.kim',
-//     content: 'hello~',
-//     emotion: 5,
-//     created_date: new Date().getTime(),
-//   },
-//   {
-//     id: 2,
-//     author: '홍길동',
-//     content: 'hello~',
-//     emotion: 5,
-//     created_date: new Date().getTime(),
-//   },
-//   {
-//     id: 3,
-//     author: '둘리',
-//     content: 'hello~',
-//     emotion: 5,
-//     created_date: new Date().getTime(),
-//   },
-//   {
-//     id: 4,
-//     author: '마이콜',
-//     content: 'hello~',
-//     emotion: 5,
-//     created_date: new Date().getTime(),
-//   },
-//   {
-//     id: 5,
-//     author: '또치',
-//     content: 'hello~',
-//     emotion: 5,
-//     created_date: new Date().getTime(),
-//   },
-// ];
+/*
+const dummyList = [
+  {
+    id: 1,
+    author: 'dev.kim',
+    content: 'hello~',
+    emotion: 5,
+    created_date: new Date().getTime(),
+  },
+  {
+    id: 2,
+    author: '홍길동',
+    content: 'hello~',
+    emotion: 5,
+    created_date: new Date().getTime(),
+  },
+  {
+    id: 3,
+    author: '둘리',
+    content: 'hello~',
+    emotion: 5,
+    created_date: new Date().getTime(),
+  },
+  {
+    id: 4,
+    author: '마이콜',
+    content: 'hello~',
+    emotion: 5,
+    created_date: new Date().getTime(),
+  },
+  {
+    id: 5,
+    author: '또치',
+    content: 'hello~',
+    emotion: 5,
+    created_date: new Date().getTime(),
+  },
+];
+*/
+
+// https://jsonplaceholder.typicode.com/comments
 
 const App = () => {
   const [data, setData] = useState([]);
-
   const dataId = useRef(0);
+
+  const getData = async () => {
+    const res = await fetch(
+      'https://jsonplaceholder.typicode.com/comments'
+    ).then((res) => res.json());
+    // console.log('res', res);
+
+    const initData = res.slice(0, 20).map((it) => {
+      return {
+        author: it.email,
+        content: it.body,
+        emotion: Math.floor(Math.random() * 5) + 1,
+        created_date: new Date().getTime(),
+        id: dataId.current++,
+      };
+    });
+    setData(initData);
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
 
   const onCreate = (author, content, emotion) => {
     const created_date = new Date().getTime();
